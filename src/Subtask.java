1167dabc1c43c0cc4f3bc43d91996d997d7b73ad
Armen Katvalyan <aakatvalyan@bk.ru>
2022-02-16 15:37:52 +0300

    feat: добавить геттер и сеттер в Subtask

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,6 +1,15 @@
 public class Subtask extends Task {
+    private Long IdEpic;
 
     public Subtask(String name, String description, String status) {
         super(name, description, status);
+    }
+
+    public Long getIdEpic() {
+        return IdEpic;
+    }
+
+    public void setIdEpic(Long idEpic) {
+        IdEpic = idEpic;
     }
 }
