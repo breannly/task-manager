876ae34c264e99efd214515bee0a402c2eada065
Armen Katvalyan <aakatvalyan@bk.ru>
2022-02-17 08:56:07 +0300

    style: Отформатировать код

--- src/Epic.java
+++ src/Epic.java
@@ -4,7 +4,7 @@ public class Epic extends Task {
     private HashMap<Integer, Subtask> subtasks;
 
     public Epic(String name, String description, String status) {
-    super(name, description, status);
+        super(name, description, status);
     }
 
     @Override
