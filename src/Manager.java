18237ab7daf3f7b4fbc407aa944c281b62afbca4
Armen Katvalyan <aakatvalyan@bk.ru>
2022-02-17 09:00:25 +0300

    feat: добавить конструктор в класс Manager

--- src/Manager.java
+++ src/Manager.java
@@ -1,7 +1,18 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Manager {
-    HashMap<Integer,Task> tasks;
-    HashMap<Integer,Subtask> subtasks;
-    HashMap<Integer,Epic> epics;
+    private HashMap<Integer, Task> tasks;
+    private HashMap<Integer, Subtask> subtasks;
+    private HashMap<Integer, Epic> epics;
+    private Long id;
+
+    Scanner scanner;
+
+    public Manager() {
+        id = 0L;
+        tasks = new HashMap<>();
+        subtasks = new HashMap<>();
+        epics = new HashMap<>();
+    }
 }
