17e28bf88db6f2216b71d6cb28f1f50c12227189
Armen Katvalyan <aakatvalyan@bk.ru>
2022-03-04 11:57:17 +0300

    feat: добавить вопрос

--- src/controller/InMemoryTaskManager.java
+++ src/controller/InMemoryTaskManager.java
@@ -212,4 +212,8 @@ public class InMemoryTaskManager implements TaskManager {
     public List<Task> history() {
         return historyManager.getHistory();
     }
+
+    //нужно ли сейчас поделить пакет controller на пакеты? например, интерфейсы в одну, а мэнеджеры в другую?
+    // есть ли какое-то правило по названиям общепринятые (как комиты)? или тут уже по наблюдениям? смотреть лайвкодинг и
+    // пробовать что-то копировать и т.п.?
 }
