d2df54ae04851f8e47eccc8d762e86d0802cd8ce
Armen Katvalyan <aakatvalyan@bk.ru>
2022-04-12 21:54:55 +0300

    feat: добавить метод по загрузке данных из файла

--- src/controller/manager/FileBackedTaskManager.java
+++ src/controller/manager/FileBackedTaskManager.java
@@ -4,13 +4,16 @@ import controller.exception.ManagerSaveException;
 import controller.imanager.HistoryManager;
 import controller.imanager.TaskManager;
 import controller.utility.Managers;
+import controller.utility.ReaderFile;
 import model.entity.Epic;
 import model.entity.Subtask;
 import model.entity.Task;
+import model.enums.TaskType;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
@@ -143,19 +146,19 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
     }
 
     private void save() throws ManagerSaveException {
-        if (!Files.exists(Path.of(file.getPath()))) throw new ManagerSaveException();
 
         try (Writer fileWriter = new OutputStreamWriter(new FileOutputStream(file.getPath()), "UTF8")) {
+            if (!Files.exists(Path.of(file.getPath()))) throw new ManagerSaveException();
             HistoryManager historyManager = Managers.getDefaultHistory();
             var sb = new StringBuilder();
             sb.append("id,type,name,status,description,epic").append("\n");
-            for (Task task : super.getTasksList()) {
+            for (Task task : getTasksList()) {
                 sb.append(task.toString()).append(",").append("\n");
             }
-            for (Epic epic : super.getEpicsList()) {
+            for (Epic epic : getEpicsList()) {
                 sb.append(epic.toString()).append(",").append("\n");
             }
-            for (Subtask subtask : super.getSubtasksList()) {
+            for (Subtask subtask : getSubtasksList()) {
                 sb.append(subtask.toString()).append(",").append("\n");
             }
             sb.append("\n");
@@ -164,5 +167,58 @@ public class FileBackedTaskManager extends InMemoryTaskManager implements TaskMa
         } catch (IOException e) {
             throw new ManagerSaveException();
         }
+    }
+
+    public static FileBackedTaskManager loadFromFile(File file) throws ManagerSaveException {
+        String fileContent = ReaderFile.readFileContents(file);
+        String[] arrayContent = fileContent.split("\\n");
+        int length = arrayContent.length;
+        List<Long> history = InMemoryHistoryManager.fromString(arrayContent[length - 1]);
+        String[] arrayTaskContent = Arrays.copyOfRange(arrayContent, 1, length - 2);
+
+        FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(file);
+
+        for (String task : arrayTaskContent) {
+            fileBackedTaskManager.fromString(task);
+        }
+
+        for(Long id : history) {
+            if (fileBackedTaskManager.getTasks().get(id) != null) {
+                fileBackedTaskManager.getTaskById(id);
+            } else if (fileBackedTaskManager.getEpics().get(id) != null) {
+                fileBackedTaskManager.getEpicById(id);
+            } else {
+                fileBackedTaskManager.getSubtaskById(id);
+            }
+        }
+
+        return fileBackedTaskManager;
+    }
+
+    private Task fromString(String value) throws ManagerSaveException {
+        String[] data = value.split(",");
+        return addTask(TaskType.valueOf(data[1]), data);
+    }
+
+    private Task addTask (TaskType type, String[] data) throws ManagerSaveException {
+        switch (type) {
+            case TASK:
+                Task task = new Task(data[2], data[4], data[3]);
+                task.setId(Long.parseLong(data[0]));
+                updateTask(task);
+                return task;
+            case EPIC:
+                Epic epic = new Epic(data[2], data[4]);
+                epic.setId(Long.parseLong(data[0]));
+                updateEpic(epic);
+                return epic;
+            case SUBTASK:
+                Subtask subtask = new Subtask(data[2], data[4], data[3], Long.parseLong(data[5]));
+                subtask.setId(Long.parseLong(data[0]));
+                Epic epicById = getEpics().get(Long.parseLong(data[5]));
+                updateSubtask(subtask);
+                return subtask;
+        }
+        return null;
     }
 }
