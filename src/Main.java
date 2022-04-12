dce02072c5a70df18721998f9f493441801ca382
Armen Katvalyan <aakatvalyan@bk.ru>
2022-04-12 21:55:39 +0300

    feat: добавить main

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,14 @@
+import controller.exception.ManagerSaveException;
+import controller.manager.FileBackedTaskManager;
 import controller.utility.Managers;
 import controller.imanager.TaskManager;
-import model.Epic;
-import model.Subtask;
-import model.Task;
+import model.entity.Epic;
+import model.entity.Subtask;
+import model.entity.Task;
+
+import java.io.File;
 
 public class Main {
-    public static void main(String[] args) {
-        final TaskManager taskManager = Managers.getDefault();
-
-        Task task1 = new Task("Уборка", "10 минут", "NEW");
-        taskManager.createTask(task1);
-
-        Task task2 = new Task("Учеба", "физика", "NEW");
-        taskManager.createTask(task2);
-
-        Epic epic1 = new Epic("Подарок", "Маше");
-        taskManager.createTask(epic1);
-
-        Epic epic2 = new Epic("Поучиться", "взять ноут");
-        taskManager.createEpic(epic2);
-        Subtask subtask1 = new Subtask("Cходить в кафе", "Поесть перед", "NEW", epic2.getId());
-        taskManager.createSubtask(subtask1);
-        Subtask subtask2 = new Subtask("Рукоделие", "Cшить сумку", "NEW", epic2.getId());
-        taskManager.createSubtask(subtask2);
-        Subtask subtask3 = new Subtask("Ткань", "Купить", "NEW", epic2.getId());
-        taskManager.createSubtask(subtask3);
-
-        taskManager.getTaskById(2L);
-        System.out.println(taskManager.getHistory());
-        taskManager.getTaskById(1L);
-        System.out.println(taskManager.getHistory());
-        taskManager.getTaskById(2L);
-        System.out.println(taskManager.getHistory());
-        taskManager.getEpicById(4L);
-        System.out.println(taskManager.getHistory());
-        taskManager.getSubtaskById(5L);
-        System.out.println(taskManager.getHistory());
-        taskManager.deleteEpicById(4L);
-        System.out.println(taskManager.getHistory());
-        taskManager.getEpicById(3L);
-
+    public static void main(String[] args) throws ManagerSaveException {
     }
 }
