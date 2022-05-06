49c1870edf493325c82815f6301420f947e00675
Armen Katvalyan <aakatvalyan@bk.ru>
2022-05-06 18:27:41 +0300

    feat: добавить клиент для работы с хранилищем

--- src/server/KVTaskClient.java
+++ src/server/KVTaskClient.java
@@ -1,2 +1,57 @@
-package server;public class KVTaskClient {
+package server;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class KVTaskClient {
+    private final String API_KEY;
+
+    public KVTaskClient(URI uri) throws IOException, InterruptedException {
+        API_KEY = register(uri);
+    }
+
+    private String register(URI uri) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "text/html")
+                .build();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        HttpResponse<String> response = client.send(request, handler);
+
+        return response.body();
+    }
+
+    public void put(String key, String json) throws IOException, InterruptedException {
+        URI uri = URI.create("http://localhost:" + KVServer.PORT + "/save/" + key + "?API_KEY=" + API_KEY);
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        client.send(request, handler);
+    }
+
+    public String load(String key) throws IOException, InterruptedException {
+        URI uri = URI.create("http://localhost:" + KVServer.PORT + "/load/" + key + "?API_KEY=" + API_KEY);
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(uri)
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .build();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        HttpResponse<String> response = client.send(request, handler);
+
+        return response.body();
+    }
 }
