3180ee15243c7906ea10a0ec12b44787c1a24f71
Armen Katvalyan <aakatvalyan@bk.ru>
2022-05-06 22:19:32 +0300

    refactor: обновить метод getDefault

--- src/controller/utility/Managers.java
+++ src/controller/utility/Managers.java
@@ -1,15 +1,26 @@
 package controller.utility;
 
 import controller.imanager.HistoryManager;
+import controller.manager.FileBackedTaskManager;
+import controller.manager.HTTPTaskManager;
 import controller.manager.InMemoryHistoryManager;
 import controller.manager.InMemoryTaskManager;
 import controller.imanager.TaskManager;
+import server.KVServer;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Managers {
     private static HistoryManager historyManager;
 
-    public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
+    public static HTTPTaskManager getDefault() throws IOException, InterruptedException {
+        return new HTTPTaskManager("http://localhost:" + KVServer.PORT + "/register");
+    }
+
+    public static TaskManager getDefaultBackedFileTaskManager() {
+        final File file = new File("file");
+        return new FileBackedTaskManager(file);
     }
 
     public static HistoryManager getDefaultHistory() {
