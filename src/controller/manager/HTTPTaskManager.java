c53ac1289a675cdc5abdab39195f4b0e4b28e229
Armen Katvalyan <aakatvalyan@bk.ru>
2022-05-06 22:25:46 +0300

    feat: добавить HTTPTaskManager

--- src/controller/manager/HTTPTaskManager.java
+++ src/controller/manager/HTTPTaskManager.java
@@ -1,15 +1,43 @@
-package server;
+package controller.manager;
 
-import controller.manager.FileBackedTaskManager;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import controller.adapter.LocalDateTimeAdapter;
+import controller.exception.IntersectionTimeException;
+import controller.exception.ManagerSaveException;
+import model.entity.Epic;
+import model.entity.Subtask;
+import model.entity.Task;
+import server.KVTaskClient;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
-public class HTTPTaskManager extends FileBackedTaskManager implements  {
-    KVTaskClient client;
+public class HTTPTaskManager extends FileBackedTaskManager {
+    private final KVTaskClient client;
+    private final Gson gson;
 
-    HTTPTaskManager(URI uri) throws IOException, InterruptedException {
+    public HTTPTaskManager(String uri) throws IOException, InterruptedException {
         super(uri);
-        client = new KVTaskClient(uri);
+        client = new KVTaskClient(URI.create(uri));
+        gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .create();
+    }
+
+    @Override
+    protected void save() throws IOException, InterruptedException {
+        for (Task task : getTasks().values()) {
+            client.put(task.getId().toString(), gson.toJson(task));
+        }
+        for (Epic epic : getEpics().values()) {
+            client.put(epic.getId().toString(), gson.toJson(epic));
+        }
+        for (Subtask subtask : getSubtasks().values()) {
+            client.put(subtask.getId().toString(), gson.toJson(subtask));
+        }
     }
 }
