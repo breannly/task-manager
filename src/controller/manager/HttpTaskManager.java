3578739691d01615ee1f799e630f2594fc88160b
Armen Katvalyan <aakatvalyan@bk.ru>
2022-05-19 23:53:17 +0300

    refactor: отрефакторить HttpTaskManager

--- src/controller/manager/HttpTaskManager.java
+++ src/controller/manager/HttpTaskManager.java
@@ -1,27 +1,22 @@
 package controller.manager;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import controller.adapter.LocalDateTimeAdapter;
-import controller.exception.IntersectionTimeException;
-import controller.exception.ManagerSaveException;
 import controller.utility.Managers;
-import model.entity.Epic;
-import model.entity.Subtask;
-import model.entity.Task;
 import server.KVTaskClient;
 
 import java.io.IOException;
 import java.net.URI;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
 
-public class HTTPTaskManager extends FileBackedTaskManager {
+public class HttpTaskManager extends FileBackedTaskManager {
+    private static final String TASK_KEY = "TASKS";
+    private static final String SUBTASK_KEY = "SUBTASKS";
+    private static final String EPIC_KEY = "EPICS";
+    private static final String HISTORY_KEY = "HISTORY";
+
     private final KVTaskClient client;
     private final Gson gson;
 
-    public HTTPTaskManager(String uri) throws IOException, InterruptedException {
+    public HttpTaskManager(String uri) throws IOException, InterruptedException {
         super(uri);
         client = new KVTaskClient(URI.create(uri));
         gson = Managers.getGson();
@@ -29,14 +24,16 @@ public class HTTPTaskManager extends FileBackedTaskManager {
 
     @Override
     protected void save() throws IOException, InterruptedException {
-        for (Task task : getTasks().values()) {
-            client.put(task.getId().toString(), gson.toJson(task));
-        }
-        for (Epic epic : getEpics().values()) {
-            client.put(epic.getId().toString(), gson.toJson(epic));
-        }
-        for (Subtask subtask : getSubtasks().values()) {
-            client.put(subtask.getId().toString(), gson.toJson(subtask));
-        }
+        if (!getTasksList().isEmpty())
+            client.put(TASK_KEY, gson.toJson(getTasksList()));
+
+        if (!getSubtasksList().isEmpty())
+            client.put(SUBTASK_KEY, gson.toJson(getSubtasksList()));
+
+        if (!getEpicsList().isEmpty())
+            client.put(EPIC_KEY, gson.toJson(getEpicsList()));
+
+        if (!getHistory().isEmpty())
+            client.put(HISTORY_KEY, gson.toJson(getHistory()));
     }
 }
